1203b89eb58e8b7c12eae4bd4e27ee3a90b933c9
Antoine Caron <antoine.caron@zenika.com>
2018-02-15 14:06:32 +0100

    Add data examples

--- index.js
+++ index.js
@@ -2,55 +2,8 @@ const _ = require('lodash');
 const math = require('mathjs');
 const chalk = require('chalk');
 const distance = require('gps-distance');
-const fs = require('fs');
 
-const points = [
-    {
-        name: 'Paris',
-        lat: 48.8790173,
-        lon: 2.328515
-    },
-    {
-        name: 'Lyon',
-        lat: 45.7621772,
-        lon: 4.8622545
-    },
-    {
-        name: 'Lille',
-        lat: 50.6364454,
-        lon: 3.0619816
-    },
-    {
-        name: 'Rennes',
-        lat: 48.1149512,
-        lon: -1.6735092
-    },
-    {
-        name: 'Nantes',
-        lat: 47.2137733,
-        lon: -1.5566822
-    },
-    {
-        name: 'Bordeaux',
-        lat: 44.8766633,
-        lon: -0.5799972999999999
-    },
-    {
-        name: 'Montréal',
-        lat: 45.5017089,
-        lon: -73.5728828
-    },
-    {
-        name: 'Singapour',
-        lat: 1.2880208,
-        lon: 103.8469787
-    },
-    {
-        name: 'Grenoble',
-        lat: 45.188529,
-        lon: 5.724524
-    },
-];
+const points = require('./capitals.json');
 
 const showTravel = listOfPoints => listOfPoints.map(p => p.name);
 
@@ -78,35 +31,36 @@ let bestFoundOrder = {
     cost: evaluateTravel(randomOrder)
 };
 
-let currentOrder = [...randomOrder]; 
-const evolution = []
+let currentOrder = {
+   order: [...randomOrder],
+    cost: bestFoundOrder.cost
+};
+const evolution = [];
 
-for(let temperature = 10000; temperature > 0; temperature--) {
-    const neighbor = randomSwap(currentOrder);
+for(let temperature = 100000; temperature > 0; temperature--) {
+    const neighbor = randomSwap(currentOrder.order);
     const neighborCost = evaluateTravel(neighbor);
 
     if(neighborCost < bestFoundOrder.cost) {
         bestFoundOrder = {
             order: [...neighbor],
             cost: neighborCost
+        };
+        currentOrder = {
+            order: [...neighbor],
+            cost: neighborCost
         }
-        currentOrder = [...neighbor];
     } else {
         const aleatoire = Math.random();
-        const estimate = math.exp(-math.divide(math.abs(neighborCost - bestFoundOrder.cost), temperature));
+        const estimate = math.exp(math.divide(currentOrder.cost - bestFoundOrder.cost, temperature));
         if(aleatoire < estimate){
-            currentOrder = [...neighbor];
+            currentOrder = {
+                order: [...neighbor],
+                cost: neighborCost
+            }
         }
     }
     evolution.push({x: temperature, y: neighborCost});
 }
 
 console.log(chalk.yellow('Best travel found', showTravel(bestFoundOrder.order), '', bestFoundOrder.cost));
-
-fs.writeFile("result.csv", evolution.map(item => `${item.y}`).join('\n'), function(err) {
-    if(err) {
-        return console.log(err);
-    }
-
-    console.log("The file was saved!");
-}); 
