40d2cbb98bf1ccbc040461b5225337b239ebf26d
Slashgear <antoine395.caron@gmail.com>
2020-11-22 22:17:32 +0100

    fixup

--- example/plot.R
+++ example/plot.R
@@ -1,6 +1,11 @@
 debug <- read.csv2("./example/debug.csv", header = TRUE, sep = ";")
+
+jpeg("rplot.jpg", width = 1000, height = "1000")
+
 plot(debug$Index, debug$BestFitness , type="n")
 
 lines(debug$Index, debug$BestFitness, col="red")
 
 lines(debug$Index, debug$CurrentFitness, col="yellow")
+
+dev.off()
