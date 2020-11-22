0805ff5d751a233e22424c0f7caa95b6d8e6fecd
Slashgear <antoine395.caron@gmail.com>
2020-11-22 22:07:08 +0100

    fix R path

--- example/plot.R
+++ example/plot.R
@@ -1,4 +1,4 @@
-debug <- read.csv2("../debug.csv", header = TRUE, sep = ";")
+debug <- read.csv2("./example/debug.csv", header = TRUE, sep = ";")
 plot(debug$Index, debug$BestFitness , type="n")
 
 lines(debug$Index, debug$BestFitness, col="red")
