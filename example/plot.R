ed2a23cfb5e01f81cb2d948bdd60f288f408e505
Antoine Caron <Slashgear@users.noreply.github.com>
2020-11-22 22:55:23 +0100

    setup ci (#1)
    * setup ci
    
    * fixup! setup ci
    
    * fixup
    
    * fixup! fixup
    
    * fixup! fixup! fixup
    
    * fix R path
    
    * fixup
    
    * fix R script
    
    * fixup

--- example/plot.R
+++ example/plot.R
@@ -1,6 +1,11 @@
-debug <- read.csv2("../debug.csv", header = TRUE, sep = ";")
+debug <- read.csv2("./example/debug.csv", header = TRUE, sep = ";")
+
+jpeg("rplot.jpg", width = 2000, height = 500)
+
 plot(debug$Index, debug$BestFitness , type="n")
 
 lines(debug$Index, debug$BestFitness, col="red")
 
 lines(debug$Index, debug$CurrentFitness, col="yellow")
+
+dev.off()
