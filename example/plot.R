1ae207bf24b050198ee8908bc0ce96d50a288e80
Slashgear <antoine395.caron@gmail.com>
2020-11-22 22:50:54 +0100

    fixup

--- example/plot.R
+++ example/plot.R
@@ -1,6 +1,6 @@
 debug <- read.csv2("./example/debug.csv", header = TRUE, sep = ";")
 
-jpeg("rplot.jpg", width = 1000, height = 1000)
+jpeg("rplot.jpg", width = 2000, height = 500)
 
 plot(debug$Index, debug$BestFitness , type="n")
 
